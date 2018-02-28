a228d63a3cb61cbe29285d463c272eee3ee9ee0a
Gavin Weiss <hasty459@gmail.com>
2018-02-28 13:26:43 -0500

    Update

--- Homework/Circle/circle.py
+++ Homework/Circle/circle.py
@@ -15,30 +15,76 @@ import simpleguitk as simplegui
 
 
 ##> Define Global Variables
-
+CircleRadius = 30
 
 
 ##> Define Helper Functions
 
 
-
 ##> Define Classes
-
 
 
 ##> Define Event Handlers
 def draw(canvas):
 
-    canvas.draw_text(letter, [32,32], 16, "Gray")
+	global CircleRadius
 
+	center = (100, 100)
+	line_width = 1
+	line_color = "grey"
+	circle = canvas.draw_circle(center, CircleRadius, line_width, line_color)
+
+	canvas.draw_text(CircleRadius, [92, 107], 9, "grey")	
+
+	return circle
+
+
+def IncreaseRadius():
+
+	global draw
+	global CircleRadius
+
+	if CircleRadius > 90:
+
+		print("Maximum")	
+
+	if CircleRadius < 90:
+
+		CircleRadius += 10
+
+	draw()
+	
+	return CircleRadius
+	return draw
+
+
+def DecreaseRadius():
+
+	global draw
+	global CircleRadius
+
+	if CircleRadius < 20:
+
+		print("Minimum")
+	
+	if CircleRadius > 20:
+	
+		CircleRadius -= 10
+	
+	draw()
+
+	return CircleRadius
+	return draw
 
 
 ##> Create Frame
-
+frame = simplegui.create_frame("SimpleGUI", 200, 200)
 
 
 ##> Register Event Handlers
-
+frame.add_button("Increase", IncreaseRadius)
+frame.add_button("Decrease", DecreaseRadius)
+frame.set_draw_handler(draw)
 
 
 ##> Start Frame and Timers
